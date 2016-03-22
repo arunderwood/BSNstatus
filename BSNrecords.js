5619df1e6164bb5c50a7c274d31cecbd55760961
Arunderwood <arunderwood@users.noreply.github.com>
2016-03-22 16:16:47 -0500

    Initial Commit

---
+++ BSNrecords.js
@@ -0,0 +1,48 @@
+{
+    "records": [
+        {
+            "SiteName": "Bizzaro Router",
+            "SiteDescription": "The central router and switch",
+            "SiteIcon": "router.png",
+            "SiteLocator": "http://192.168.100.1",
+            "SiteURL": "http://192.168.100.1"
+        }, {
+            "SiteName": "Google",
+            "SiteDescription": "Search Engine",
+            "SiteIcon": "google.png",
+            "SiteLocator": "https://www.google.com",
+            "SiteURL": "www.google.com"
+        },
+        {
+            "SiteName": "Facebook",
+            "SiteDescription": "Social Media",
+            "SiteIcon": "facebook.png",
+            "SiteLocator": "https://facebook.com",
+            "SiteURL": "Facebook.com"
+        }, {
+            "SiteName": "ESPN",
+            "SiteDescription": "Sports",
+            "SiteIcon": "espn.png",
+            "SiteLocator": "http://espn.com",
+            "SiteURL": "ESPN.com"
+        }, {
+            "SiteName": "Github",
+            "SiteDescription": "Source Control",
+            "SiteIcon": "github.png",
+            "SiteLocator": "https://github.com",
+            "SiteURL": "Github.com"
+        }, {
+            "SiteName": "Brackets",
+            "SiteDescription": "Modern Text editor",
+            "SiteIcon": "brackets.png",
+            "SiteLocator": "http://brackets.io/",
+            "SiteURL": "Brackets.io"
+        }, {
+            "SiteName": "SUPER. HOT.",
+            "SiteDescription": "SUPER...HOT!",
+            "SiteIcon": "superhot.png",
+            "SiteLocator": "https://superhotgame.com",
+            "SiteURL": "Superhotgame.com"
+        }
+]
+}
