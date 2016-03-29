ce5ded565eeee7c73afa6bcafa7bab75d746cd96
Arunderwood <arunderwood@users.noreply.github.com>
2016-03-29 16:37:51 -0500

    Removed FeedEk, it can't handle Atom feeds

--- scripts/bsnStatus.js
+++ scripts/bsnStatus.js
@@ -1,7 +1,1 @@
-function dodivRss() {
-    $('#divRss').FeedEk({
-        FeedUrl: 'https://github.com/arunderwood/BSNstatus/commits/master.atom',
-        MaxCount: 2,
-        DateFormat: 'MM-DD-YYYY'
-    });
-}
+
